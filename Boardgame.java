f4a58be916d36dfaf74a91fe25aa5e9ce490472a
mskim9967 <54897403+mskim9967@users.noreply.github.com>
2020-12-20 19:00:25 +0900

    Update Boardgame.java

--- Boardgame.java
+++ Boardgame.java
@@ -2,31 +2,27 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    final int B = 5;
+    final int LEN = 5, RIGHT = 1, LEFT = -1, UP = -LEN, DOWN = LEN;
     int board;
     byte[] cache;
-    final int[] move1 = {1, 1, B, B, -1, -1, -B, -B, 1, -B, -1, B};
-    final int[] move2 = {B, -B, -1, 1, -B, B, 1, -1, 0, 0, 0, 0};
+    final int[] MOVE1 = {RIGHT, RIGHT, DOWN, DOWN, LEFT, LEFT, UP, UP, RIGHT, UP, LEFT, DOWN};
+    final int[] MOVE2 = {DOWN, UP, LEFT, RIGHT, UP, DOWN, RIGHT, LEFT, 0, 0, 0, 0};
 
     Main(BufferedReader br) throws Exception {
-        board = 0;
-        for(int i = 0; i < B; i++) {
-            String line = br.readLine();
-            for(int j = 0; j < B; j++)
-                if(line.charAt(j) == '#') board = (1 << (i * B + j)) | board;
-        }
-        cache = new byte[1 << (B * B)];
-        //for(int i = 0; i < cache.length; i++)
-            //System.out.print(cache[i]);
+        cache = new byte[1 << LEN * LEN];
     }
     
     boolean isPossible(int board, int pos, int block) {
-        pos += move1[block];
-        if(pos < 0 || pos >= B * B)    return false;
+        pos += MOVE1[block];
+        if((MOVE1[block] == RIGHT || MOVE1[block] == LEFT) && 
+            (pos - MOVE1[block]) / LEN != pos / LEN)    return false;
+        if(pos < 0 || pos >= LEN * LEN)    return false;
         if(((board >> pos) & 1) == 1)    return false;
         
-        pos += move2[block];
-        if(pos < 0 || pos >= B * B)    return false;
+        pos += MOVE2[block];
+        if((MOVE2[block] == RIGHT || MOVE2[block] == LEFT) && 
+            (pos - MOVE2[block]) / LEN != pos / LEN)    return false;
+        if(pos < 0 || pos >= LEN * LEN)    return false;
         if(((board >> pos) & 1) == 1)    return false;
         
         return true;
@@ -34,32 +30,36 @@ public class Main{
 
     int update_board(int board, int pos, int block) {
         board = (1 << pos) | board;
-
-        pos += move1[block];
+       
+        pos += MOVE1[block];
         board = (1 << pos) | board;
-        
-        pos += move2[block];
-        board = (1 << pos) | board;
-        
-        return board;
+       
+        pos += MOVE2[block];
+        return (1 << pos) | board;
     }
 
     byte isPlayerWin(int board) {
         if(cache[board] != 0)  return cache[board];
 
-        for(int i = 0; i < B * B; i++) {
+        for(int i = 0; i < LEN * LEN; i++) {
             if(((board >> i) & 1) == 1) continue;
             
-            for(int k = 0; k < 12; k++) {
-                if(isPossible(board, i, k)) 
-                    if(isPlayerWin(update_board(board, i, k)) == -1)
+            for(int block = 0; block < MOVE1.length; block++) {
+                if(isPossible(board, i, block)) 
+                    if(isPlayerWin(update_board(board, i, block)) == -1)
                         return cache[board] = 1;
             }
         }
         return cache[board] = -1;
     }
 
-    void write_answer(BufferedWriter bw) throws Exception {
+    void write_answer(BufferedReader br, BufferedWriter bw) throws Exception {
+        board = 0;
+        for(int i = 0; i < LEN; i++) {
+            String line = br.readLine();
+            for(int j = 0; j < LEN; j++)
+                if(line.charAt(j) == '#') board = (1 << (i * LEN + j)) | board;
+        }
         bw.write(isPlayerWin(board) == 1 ? "WINNING\n" : "LOSING\n");
     }
     
@@ -67,10 +67,9 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int testCase = Integer.parseInt(br.readLine());
-        for(int i = 0; i < testCase; i++) {
-            Main d = new Main(br);
-            d.write_answer(bw);
-        }
+        Main d = new Main(br);
+        for(int i = 0; i < testCase; i++)
+            d.write_answer(br, bw);
         bw.flush(); bw.close(); br.close();
     }
 }
