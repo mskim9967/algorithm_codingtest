941aa139bc43112b831a8eda8a9df9773653427a
mskim9967 <54897403+mskim9967@users.noreply.github.com>
2020-12-20 16:03:05 +0900

    Update Boardgame.java

--- Boardgame.java
+++ Boardgame.java
@@ -4,7 +4,7 @@ import java.io.*;
 public class Main{
     final int B = 5;
     int board;
-    Boolean[] cache;
+    byte[] cache;
     final int[] move1 = {1, 1, B, B, -1, -1, -B, -B, 1, -B, -1, B};
     final int[] move2 = {B, -B, -1, 1, -B, B, 1, -1, 0, 0, 0, 0};
 
@@ -15,88 +15,52 @@ public class Main{
             for(int j = 0; j < B; j++)
                 if(line.charAt(j) == '#') board = (1 << (i * B + j)) | board;
         }
-        cache = new Boolean[1 << B * B];
+        cache = new byte[1 << (B * B)];
+        //for(int i = 0; i < cache.length; i++)
+            //System.out.print(cache[i]);
     }
     
-    boolean isPossible(int board, int pos, int type) {
-        if(isXfirst[type]) x += moveX[type];
-        else    y += moveY[type];
-        if(x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE)    return false;
-        if((board >> (y * BOARD_SIZE + x) & 1) == 1)    return false;
+    boolean isPossible(int board, int pos, int block) {
+        pos += move1[block];
+        if(pos < 0 || pos >= B * B)    return false;
+        if(((board >> pos) & 1) == 1)    return false;
         
-        if(isXfirst[type]) y += moveY[type];
-        else    x += moveX[type];
-        if(x < 0 || x >= BOARD_SIZE || y < 0 || y >= BOARD_SIZE)    return false;
-        if((board >> (y * BOARD_SIZE + x) & 1) == 1)    return false;
+        pos += move2[block];
+        if(pos < 0 || pos >= B * B)    return false;
+        if(((board >> pos) & 1) == 1)    return false;
         
         return true;
     }
 
-    int update_board(int board, int x, int y, int type) {
-        board = (1 << (y * BOARD_SIZE + x)) | board;
+    int update_board(int board, int pos, int block) {
+        board = (1 << pos) | board;
+
+        pos += move1[block];
+        board = (1 << pos) | board;
         
-        if(type == 0) {
-            board = (1 << (y * BOARD_SIZE + ++x)) | board;
-            board = (1 << (++y * BOARD_SIZE + x)) | board;
-        }
-        else if(type == 1) {
-            board = (1 << (y * BOARD_SIZE + ++x)) | board;
-            board = (1 << (--y * BOARD_SIZE + x)) | board;
-        }
-        else if(type == 2){
-            board = (1 << (++y * BOARD_SIZE + x)) | board;
-            board = (1 << (y * BOARD_SIZE + --x)) | board;
-        }
-        else if(type == 3) {
-            board = (1 << (++y * BOARD_SIZE + x)) | board;
-            board = (1 << (y * BOARD_SIZE + ++x)) | board;
-        }
-        else if(type == 4) {
-            board = (1 << (y * BOARD_SIZE + --x)) | board;
-            board = (1 << (--y * BOARD_SIZE + x)) | board;
-        }
-        else if(type == 5) {
-            board = (1 << (y * BOARD_SIZE + --x)) | board;
-            board = (1 << (++y * BOARD_SIZE + x)) | board;
-        }
-        else if(type == 6) {
-            board = (1 << (--y * BOARD_SIZE + x)) | board;
-            board = (1 << (y * BOARD_SIZE + ++x)) | board;
-        }
-        else if(type == 7) {
-            board = (1 << (--y * BOARD_SIZE + x)) | board;
-            board = (1 << (y * BOARD_SIZE + --x)) | board;
-        }
-        else if(type == 8)
-            board = (1 << (y * BOARD_SIZE + ++x)) | board;
-        else if(type == 9)
-            board = (1 << (--y * BOARD_SIZE + x)) | board;
-        else if(type == 10)
-            board = (1 << (y * BOARD_SIZE + --x)) | board;
-        else if(type == 11)
-            board = (1 << (++y * BOARD_SIZE + x)) | board;
+        pos += move2[block];
+        board = (1 << pos) | board;
+        
         return board;
     }
 
-    Boolean isPlayerWin(int board) {
-        if(cache[board] != null)  return cache[board];
-        Boolean ret = false;
+    byte isPlayerWin(int board) {
+        if(cache[board] != 0)  return cache[board];
+
         for(int i = 0; i < B * B; i++) {
-                if(((board >> (i)) & 1)== 1) continue;
-                
-                for(int k = 0; k < 12; k++) {
-                    if(isPossible(board, i, k)) {
-                        ret = !isPlayerWin(update_board(board, j, i, k));
-                        if(ret == true) return cache[board] = ret;
-                    }
-                }
+            if(((board >> i) & 1) == 1) continue;
+            
+            for(int k = 0; k < 12; k++) {
+                if(isPossible(board, i, k)) 
+                    if(isPlayerWin(update_board(board, i, k)) == -1)
+                        return cache[board] = 1;
             }
         }
-        return cache[board] = ret;
+        return cache[board] = -1;
     }
 
     void write_answer(BufferedWriter bw) throws Exception {
-        bw.write(isPlayerWin(board) ? "WINNING\n" : "LOSING\n");
+        bw.write(isPlayerWin(board) == 1 ? "WINNING\n" : "LOSING\n");
     }
     
     public static void main(String[] args) throws Exception {
