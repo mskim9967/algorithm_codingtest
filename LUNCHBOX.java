7812d5f46c6d86da582aa7807737634084d172c3
repl.it user <replituser@example.com>
2020-12-29 10:07:00 +0000

    add

--- LUNCHBOX.java
+++ LUNCHBOX.java
@@ -2,31 +2,46 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
+    public class Time implements Comparable<Time> {
+        int heat, eat;
+        Time(int heat, int eat) { this.heat = heat; this.eat = eat; }
+        public int compareTo(Time o) { 
+            if(this.eat < o.eat) return 1;
+            else if(this.eat == o.eat) return 0;
+            else return -1;
+        }
+    }
+    Time[] time;
+    int[] heatSum;
     int n;
-    PriorityQueue<Integer> pq;
     
     Main(BufferedReader br) throws Exception {
         n = Integer.parseInt(br.readLine());
-        pq = new PriorityQueue<Integer>();
+        time = new Time[n];
+        heatSum = new int[n];
 
         StringTokenizer token = new StringTokenizer(br.readLine(), " ");   
         for(int i = 0; i < n; i++) 
-            pq.add(Integer.parseInt(token.nextToken()));
+            time[i] = new Time(Integer.parseInt(token.nextToken()), 0);
+        
+        token = new StringTokenizer(br.readLine(), " ");
+        for(int i = 0; i < n; i++)
+            time[i].eat = Integer.parseInt(token.nextToken());
     }
     
-    int calc_minCost() {
+    int calc_minLunchTime() {
+        Arrays.sort(time);
+        
         int ret = 0;
-        while(pq.size() != 1) {
-            int a = pq.poll();
-            int b = pq.poll();
-            ret += a + b;
-            pq.add(a + b);
+        for(int i = 0; i < n; i++) {
+            heatSum[i] += time[i].heat + ((i != 0) ? heatSum[i - 1] : 0);
+            ret = Math.max(ret, heatSum[i] + time[i].eat);
         }
         return ret;
     }
 
     void write_answer(BufferedWriter bw) throws Exception {
-        bw.write(calc_minCost() + "\n");
+        bw.write(calc_minLunchTime() + "\n");
     }
     
     public static void main(String[] args) throws Exception {
