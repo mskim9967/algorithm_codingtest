8c54c56f59fbad1c930379b9d0ac089bfab579bf
repl.it user <replituser@example.com>
2021-01-30 04:44:34 +0000

    add

--- Main.java
+++ Main.java
@@ -2,55 +2,72 @@ import java.util.*;
 import java.io.*;
 
 public class Main{
-    class GP { 
-        double x, y, r; 
-        GP(double x, double y, double r) { this.x = x; this.y = y; this.r = r;}
-    }
-    final double R = 8.0;
-    int n;
-    GP[] gp;
-    double[] pos;
+    int n, k;
+    int[] arr, sumMod, cnt, cache, idx;
 
-    Main(BufferedReader br) throws Exception {
-        n = Integer.parseInt(br.readLine());
-        gp = new GP[n];
-        pos = new double[n];
-        for(int i = 0; i < n; i++) {
-            StringTokenizer token = new StringTokenizer(br.readLine(), " ");   
-            gp[i] = new GP(Double.parseDouble(token.nextToken()),
-                Double.parseDouble(token.nextToken()), Double.parseDouble(token.nextToken()));
+    Main() {
+        arr = new int[100001];
+        sumMod = new int[100001];
+        cnt = new int[100000];
+        cache = new int[100001];
+        idx = new int[100000];        
+        Arrays.fill(idx, -1);
+        Arrays.fill(cache, -1);
+    }
+
+    void init(BufferedReader br) throws Exception {
+        StringTokenizer token = new StringTokenizer(br.readLine(), " "); 
+        n = Integer.parseInt(token.nextToken());
+        k = Integer.parseInt(token.nextToken());
+
+        token = new StringTokenizer(br.readLine(), " "); 
+        for(int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(token.nextToken());
+            sumMod[i] = (sumMod[i - 1] + arr[i]) % k;
         }
     }
 
-    double trans_pointToPos(double x, double y) {
-        return Math.asin(Math.sqrt(x * x + (y - R) * (y - R)) / (2 * R)) * 2 * R;
+    void clear() {
+        Arrays.fill(arr, 0);
+        Arrays.fill(sumMod, 0);
+        Arrays.fill(cnt, 0);
+        Arrays.fill(cache, -1);
+        Arrays.fill(idx, -1);
     }
 
-    double get_l(double r) {
-        return Math.asin(r / (2 * R)) * 2 * R;
+    int one() {
+        int ret = 0;
+        for(int i = 0; i <= n; i++) 
+            cnt[sumMod[i]]++;
+        for(int i = 0; i < k; i++)
+            ret = (int)((ret + (long)cnt[i] * (cnt[i] - 1) / 2) % 20091101);
+        return ret;
     }
 
-    void write_answer(BufferedWriter bw) throws Exception {
-        bw.write("total length: " + Math.PI * 2 * R + "\n");
-        for(int i = 0; i < n; i++) {
-            pos[i] = trans_pointToPos(gp[i].x, gp[i].y);
-            bw.write("gp[" + i + "]: " + pos[i]  + "\n");
-            bw.write((pos[i] - get_l(gp[i].r) +(Math.PI * 2 * R)) % (Math.PI * 2 * R) + 
-            " ~ gp[" + i + "] ~ "
-             + (pos[i] + get_l(gp[i].r)) %  (Math.PI * 2 * R) + "\n");
-            
+    int two() {
+        for(int i = 0; i <= n; i++) {
+            cache[i] = (i == 0) ? 0 : cache[i - 1];
+            if(idx[sumMod[i]] != -1)
+                cache[i] = Math.max(cache[i], 1 + cache[idx[sumMod[i]]]);
+            idx[sumMod[i]] = i;
         }
+        return cache[n];
+    }
+
+    void solve(BufferedReader br, BufferedWriter bw) throws Exception {
+        init(br);
+        bw.write(one() + " " + two() + "\n");
+        clear();
     }
     
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txgi t")));
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
         //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int testCase = Integer.parseInt(br.readLine());
-        for(int i = 0; i < testCase; i++) {
-            Main l = new Main(br);
-            l.write_answer(bw);
-        }
+        Main c = new Main();
+        for(int i = 0; i < testCase; i++)
+            c.solve(br, bw);
         bw.flush(); bw.close(); br.close();
     }
 }
